e29bf8b3f7ce7cad00dd6b64abfaac8822344098
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2026-02-21 22:16:09 +0530

    BookingDTO with additional candidate details and start time

--- src/main/java/edu/icet/model/DTO/BookingDTO.java
+++ src/main/java/edu/icet/model/DTO/BookingDTO.java
@@ -2,9 +2,16 @@ package edu.icet.model.DTO;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class BookingDTO {
-    private Long candidateId;
     private Long slotId;
+    private LocalDateTime startTime;
+    private String candidateName;
+    private String candidateEmail;
+    private String idNumber;
+    private String contact;
+    private String specialization;
 }
 
