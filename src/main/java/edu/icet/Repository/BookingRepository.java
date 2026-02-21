8b332d34df33d4e379c8c596b67fe818f75f54eb
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2026-02-21 22:16:55 +0530

    add method to find booking by slot ID in BookingRepository

--- src/main/java/edu/icet/Repository/BookingRepository.java
+++ src/main/java/edu/icet/Repository/BookingRepository.java
@@ -4,7 +4,9 @@ import edu.icet.model.Entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     boolean existsByCandidateIdAndSlotStartTime(Long candidateId, LocalDateTime startTime);
+    Optional<Booking> findBySlotId(Long slotId);
 }
